79ab2c4b66743e68706a00ae0dccb45ba64e35a2
sunzoo <tnver@naver.com>
2022-10-25 18:54:40 +0900

    221025 0654

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+    //메인 컨테이너
+const mainContainer = document.querySelector('.mainContainer'),
+    //이미지 리스트
+    img = mainContainer.querySelectorAll('img');
+
+
+    //현재 슬라이드 인덱스
+let idx = 0,
+    // 슬라이드 인덱스 종료값.
+    imgEndIdx = imgLi.length-1;
+
+
+    //무한 슬라이드
+setInterval(function () {
+}, 3000);
