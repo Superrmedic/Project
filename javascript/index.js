632504b4c0a312d8c3682cb2320f31d9919b6cd0
OOZNUS <tnver@naver.com>
2022-11-07 01:23:10 +0900

    dd

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const categoryContainer = document.querySelector('.categoryContainer');
+const categoryHover = categoryContainer.querySelectorAll('.categoryHover'); // 카테고리 리스트들
--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-    //메인 컨테이너
-const mainContainer = document.querySelector('.mainContainer'),
-    //이미지 리스트
-    img = mainContainer.querySelectorAll('img');
-
-
