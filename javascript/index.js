d3bd360bcda676f912def1c1241ee920dbaa2c33
OOZNUS <tnver@naver.com>
2022-11-09 01:20:21 +0900

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,9 @@
 'use strict';
 
-const categoryContainer = document.querySelector('.categoryContainer');
+const body = document.querySelector('body');
+const categoryContainer = body.querySelector('.categoryContainer');
 const categoryHover = categoryContainer.querySelectorAll('.categoryHover'); // 카테고리 리스트들
+
+const mainContainer = body.querySelector('.mainContainer');
+const slideContainer = mainContainer.querySelector('.slideContainer');
+const imgLi = slideContainer.querySelectorAll('.imgLi img');
