74725253aac356064bdc2c52176ba24e608bf9d0
최병현 <>
2022-11-21 21:50:52 +0900

    //

--- html/Community/JS/community_faq.js
+++ html/Community/JS/community_faq.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const notice = document.querySelector('.notice'),
+const notice = document.querySelector('.c_noticed'),
   li = notice.getElementsByTagName('li');
 
 let tmp = li[0];
@@ -9,20 +9,20 @@ notice.addEventListener('click', function (e) {
   const list = e.target.parentNode;
 
   function tmpRemove() {
-    tmp.classList.add('hidden');
+    tmp.classList.add('c_hidden');
   }
 
-  if (list.classList.contains('service_list')) {
+  if (list.classList.contains('c_service_list')) {
     tmpRemove();
-    list.classList.remove('hidden');
+    list.classList.remove('c_hidden');
     tmp = list;
   }
 });
 
-const menucategory = document.querySelector('.menucategory'),
+const menucategory = document.querySelector('.c_menucategory'),
   accordion = menucategory.querySelector('.accordion li');
 
 accordion.addEventListener('click', function () {
   this.classList.remove('selected');
-  this.classList.add('selected');
-})
+  // this.classList.add('selected');
+});
