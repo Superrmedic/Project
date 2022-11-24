30afd47b2b26f8a9d4d8274f4a8de10a20b93f3d
User -2022ANZSX\Administrator <killopeight@gmail.com>
2022-11-24 18:07:20 +0900

    no message

--- html/Product/js/product_shopping_payment.js
+++ html/Product/js/product_shopping_payment.js
@@ -12,6 +12,7 @@ const infoContainer = document.querySelector('.shop_oderinfo'),
     optionEmail = selectEmail.getElementsByTagName('option'),
     selfDelivery = document.querySelector('.shop_deliveryplz'),
     selfDeliverybox = document.querySelector('.selfDeliverybox'),
+    optionDelivery = selfDelivery.getElementsByTagName('option'),
     payContainer = document.querySelector('.couponpaydiv'),
     cardTab = payContainer.querySelector('.cardTab'),
     paycontainer_easy = payContainer.querySelector('.paycontainer_easy'),
@@ -19,15 +20,20 @@ const infoContainer = document.querySelector('.shop_oderinfo'),
     cardSelect = payContainer.getElementsByTagName('select'),
     checkBox = document.querySelector('.orderprivacybox'),
     checkList = checkBox.getElementsByTagName('input'),
-    pointBtn = document.querySelector('.pointBtn');
+    pointBtn = document.querySelector('.pointBtn'),
+    totalBtn = document.querySelector('.totalorderbutton');
+
+let Checkup = true;
 
 function checkAlert(check, input, alert) {
     if (!check.test(input.value)) {
         input.style.border = "1px solid rgb(182, 182, 182)"
         alert.style.display = "none";
+        Checkup = 1;
     } else {
         input.style.border = "1px solid red"
         alert.style.display = "block";
+        Checkup = 0;
     }
 }       // 틀린 정보 입력시 alert 주는 함수
 
@@ -38,6 +44,7 @@ function selfInput(self, box) {
         box.style.display = 'none';
     }
 }       // 직접 입력시 style 지정 함수
+
 
 infoContainer.addEventListener('input', (e) => {
     let checkKor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
@@ -53,6 +60,8 @@ infoContainer.addEventListener('input', (e) => {
         checkAlert(checkKor, emailInput, alertEmail);
     } else if (e.target.classList.contains('nameInput')) {
         checkAlert(checkNum, nameInput, alertName);
+    } else if (e.target.classList.contains('selfTextbox')) {
+        checkAlert(checkKor, selfTextbox, alertEmail);
     }
 });     // 주문자 정보 유효성 검사
 
@@ -62,6 +71,7 @@ selectEmail.addEventListener('click', (e) => {
 });
 
 selfDelivery.addEventListener('click', (e) => {
+    optionDelivery[0].disabled = true;
     selfInput(e, selfDeliverybox);
 });     // 직접 입력시 text창 나오게
 
@@ -69,19 +79,21 @@ let tmp = cardTab;
 payContainer.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
         e.preventDefault();
-        e.target.classList.add('bordButton');
-        tmp.classList.remove('bordButton');
-        tmp = e.target;
+        if (e.target != tmp) {
+            e.target.classList.add('bordButton');
+            tmp.classList.remove('bordButton');
+            tmp = e.target;
 
-        if (e.target.classList.contains('cardTab')) {
-            paycontainer_card.style.display = 'flex';
-            paycontainer_easy.style.display = 'none';
-        } else if (e.target.classList.contains('easyTab')) {
-            paycontainer_easy.style.display = 'block';
-            paycontainer_card.style.display = 'none';
-        } else {
-            paycontainer_card.style.display = 'none';
-            paycontainer_easy.style.display = 'none';
+            if (e.target.classList.contains('cardTab')) {
+                paycontainer_card.style.display = 'flex';
+                paycontainer_easy.style.display = 'none';
+            } else if (e.target.classList.contains('easyTab')) {
+                paycontainer_easy.style.display = 'block';
+                paycontainer_card.style.display = 'none';
+            } else {
+                paycontainer_card.style.display = 'none';
+                paycontainer_easy.style.display = 'none';
+            }
         }
     }
 });     // 결제 수단 탭 display
@@ -98,7 +110,10 @@ cardSelect[0].addEventListener('click', (e) => {
     }
 });     // 신용카드 select 설정 탭
 
-pointBtn.addEventListener('click', () => alert('사용 가능한 포인트가 없습니다!'));
+pointBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('사용 가능한 적립금이 없습니다!');
+});
 
 checkBox.addEventListener('click', (e) => {
     for (let i = 1; i <= checkList.length - 1; i++) {
@@ -113,3 +128,20 @@ checkBox.addEventListener('click', (e) => {
         }
     }
 });     // 개인정보 약관 동의
+
+totalBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (nameInput.value === '' || !Checkup || !selectEmail.value || (selectEmail.value === '5' && !selfTextbox.value)) {
+        alert('입력 정보를 다시 확인해 주세요!');
+    } else if (selfDelivery.value === '0' || (selfDelivery.value === '5' && !selfDeliverybox.value)) {
+        alert('배송 정보를 다시 확인해 주세요!');
+    } else if (cardSelect[0].value === '0') {
+        alert('결제 수단을 선택해 주세요!');
+    } else if (!checkList[0].checked && (!checkList[1].checked || !checkList[2].checked || !checkList[3].checked)) {
+        alert('개인 정보 수집 제공에 동의해 주세요!');
+    } else if (phoneInput.value.length != 9) {
+        alert('휴대폰 번호를 8자리 입력해 주세요!')
+    } else {
+        window.open('./product_popup.html', '_self');
+    }
+});     // 버튼 클릭시 유효성 검사
