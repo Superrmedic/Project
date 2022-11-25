26275d6769571a0c9c7c33e4b1f3fbddec4dccd6
최병현 <>
2022-11-25 09:49:38 +0900

    //

--- html/Community/JS/community_review_write.js
+++ html/Community/JS/community_review_write.js
@@ -3,7 +3,9 @@
 const title = document.querySelector('.c_review_title'),
   pwBox = document.querySelector('.inputPassword > input'),
   warning = document.getElementsByTagName('span'),
+  file = document.querySelector('.files > input'),
   textarea = document.querySelector('textarea'),
+
   confirm = document.querySelector('button');
 
 let checkPW;
@@ -18,6 +20,8 @@ confirm.addEventListener('click', function () {
     alert('제목을 입력해 주세요.')
   } else if (!textarea.value) {
     alert('내용을 입력해 주세요.')
+  } else if (!file.value) {
+    alert('파일을 첨부해 주세요.')
   } else if (!checkPW) {
     alert('비밀번호를 입력해 주세요.')
   }
