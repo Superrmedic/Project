07e7a0fd0d50e979adac280e3433c9140b545ade
최병현 <>
2023-02-20 18:51:50 +0900

    [update] 팝업창 수정

--- html/Community/JS/communitu_review_popup.js
+++ html/Community/JS/communitu_review_popup.js
@@ -1,4 +1,29 @@
-const closeBtn = modal.querySelector(".close-area")
-closeBtn.addEventListener("click", e => {
-  modal.style.display = "none"
+const body = document.querySelector("body"),
+  popupOpen = document.querySelector(".c_img_container .img"),
+  popupPage = document.querySelector(".popup_page"),
+  closeBtn = document.querySelector(".close-area"),
+  openReply = document.querySelector(".fa-caret-down"),
+  replyDetail = document.querySelector(".replyContent"),
+  closeReply = document.querySelector(".fa-caret-up");
+
+popupOpen.addEventListener("click", () => {
+  popupPage.style.display = "flex";
+  body.style.overflow = "hidden";
+})
+
+closeBtn.addEventListener("click", () => {
+  popupPage.style.display = "none";
+  body.style.overflow = "auto";
+})
+
+openReply.addEventListener("click", () => {
+  openReply.style.display = "none";
+  replyDetail.style.display = "block";
+  closeReply.style.display = "inline-block";
+})
+
+closeReply.addEventListener("click", () => {
+  closeReply.style.display = "none";
+  replyDetail.style.display = "none";
+  openReply.style.display = "inline-block";
 })
--- html/Community/JS/community_review.js
+++
@@ -1,5 +0,0 @@
-const img_container = document.getElementById(".c_img_container");
-
-img_container.addEventListener("click", e =>{
-  img_container.styl
-})
